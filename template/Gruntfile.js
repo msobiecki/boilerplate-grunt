578b7e13b6b4b60a498bb941edc6e4318408906c
Michał Sobiecki <michalsobiecki@yahoo.com>
2017-11-07 12:25:36 +0100

    Save-point ES6

--- template/Gruntfile.js
+++ template/Gruntfile.js
@@ -1,27 +1,48 @@
 module.exports = function (grunt) {
-  // load all grunt tasks
   require('grunt-task-loader')(grunt);
   require('time-grunt')(grunt);
 
-  //lista zewnetrznych skryptów
-  var vendorScripts = [
-    'node_modules/jquery/dist/jquery.min.js'
-  ];
-
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     uglify: {
       dist: {
-        src: [vendorScripts, 'source/*.js', 'source/**/*.js', '!scripts/main.min.js'],
-        dest: 'scripts/main.min.js'
-      },
+        files: {
+          'scripts/main.min.js': 'scripts/main.min.js'
+        },
+        output: {
+          comments: false
+        }
+      }
+    },
+    browserify: {
       dev: {
-        src: [vendorScripts, 'source/*.js', 'source/**/*.js', '!scripts/main.min.js'],
-        dest: 'scripts/main.min.js',
+        files: {
+          'scripts/main.min.js': ['source/**/*.js']
+        },
         options: {
-          beautify: true,
-          compress: false,
-          mangle: false,
-          sourceMap: true
+          transform: [
+            ['babelify', {
+              presets: "env"
+            }]
+          ],
+          browserifyOptions: {
+            debug: true
+          }
+        }
+      },
+      dist: {
+        files: {
+          'scripts/main.min.js': ['source/**/*.js']
+        },
+        options: {
+          transform: [
+            ['babelify', {
+              presets: "env"
+            }]
+          ],
+          browserifyOptions: {
+            debug: false
+          }
         }
       }
     },
@@ -29,12 +50,13 @@ module.exports = function (grunt) {
       dist: {
         options: {
           outputStyle: 'compressed',
-          lineNumbers: false
+          lineNumbers: false,
+          sourceMap: false
         },
         files: [{
           expand: true,
           cwd: 'source',
-          src: ['*.scss', '*.sass'],
+          src: ['*.sass'],
           dest: 'css',
           ext: '.css'
         }]
@@ -48,24 +70,45 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: 'source',
-          src: ['*.scss', '*.sass'],
+          src: ['*.sass'],
           dest: 'css',
           ext: '.css'
         }]
       },
-      tasks: ['autoprefixer']
     },
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 versions', 'ie 8', 'ie 9', 'Firefox ESR', 'Opera 12.1']
+    postcss: {
+      dev: {
+        options: {
+          map: true,
+          processors: [
+            require('autoprefixer')({
+              browsers: ['last 2 version']
+            })
+          ]
+        },
+        dist: {
+          src: 'css/main.min.css',
+          dest: 'css/main.min.css'
+        }
       },
       dist: {
-        src: 'css/main.css',
-        dest: 'css/main.css'
+        options: {
+          map: false,
+          processors: [
+            require('autoprefixer')({
+              browsers: ['last 2 version']
+            })
+          ]
+        },
+        dist: {
+          src: 'css/main.min.css',
+          dest: 'css/main.min.css'
+        }
       }
     },
+
     pug: {
-      dev: {
+      compile: {
         options: {
           pretty: true,
           data: {
@@ -79,23 +122,9 @@ module.exports = function (grunt) {
           dest: '',
           ext: '.html'
         }]
-      },
-      dist: {
-        options: {
-          data: {
-            debug: false
-          },
-          pretty: false
-        },
-        files: [{
-          expand: true,
-          cwd: 'source',
-          src: ['*.pug', '!_*.pug'],
-          dest: '',
-          ext: '.html'
-        }]
       }
     },
+
     connect: {
       all: {
         options: {
@@ -106,6 +135,7 @@ module.exports = function (grunt) {
         }
       }
     },
+
     imagemin: {
       dynamic: {
         files: [{
@@ -116,24 +146,22 @@ module.exports = function (grunt) {
         }]
       }
     },
+
     watch: {
       scripts: {
-        files: ['!scripts/main.min.js', 'source/*.js', 'source/**/*.js',],
-        tasks: ['uglify:dev'],
-        options: {
-          spawn: false
-        }
+        files: ['source/**/*.js'],
+        tasks: ['browserify:dev']
       },
       sass: {
         files: ['source/*.sass', 'source/**/*.sass'],
-        tasks: ['sass:dev', 'autoprefixer'],
+        tasks: ['sass:dev', 'postcss:dev'],
         options: {
           spawn: false
         }
       },
       pug: {
         files: ['source/*.pug', 'source/**/*.pug'],
-        tasks: ['pug:dev'],
+        tasks: ['pug:compile'],
         options: {
           spawn: false,
           pretty: true
@@ -156,18 +184,17 @@ module.exports = function (grunt) {
       dev: {
         tasks: ['watch:scripts', 'watch:sass']
       },
+      build: {
+        tasks: ['browserify:dist', 'uglify:dist', 'sass:dist', 'postcss:dist', 'concurrent:optimal']
+      },
       optimal: {
         tasks: ['imagemin']
       }
     }
   });
 
-  //grunt task developerski po wdrożeniu, kompilacja js i sass
-  grunt.registerTask('default', ['concurrent:dev']);
-  //grunt task generujący zminimalizowany kod na produkcję
-  grunt.registerTask('build', ['uglify:dist', 'sass:dist', 'concurrent:optimal']);
-  //grunt task do przeznaczony do tworzenia szablonu, uruchamia live reload server
-  grunt.registerTask('server', ['concurrent:front']);
-  //grunt optymalizacja zdjec
+  grunt.registerTask('dev', ['concurrent:dev']);
+  grunt.registerTask('prod', ['concurrent:prod']);
+  grunt.registerTask('build', ['concurrent:build']);
   grunt.registerTask('optimal', ['concurrent:optimal']);
 };
